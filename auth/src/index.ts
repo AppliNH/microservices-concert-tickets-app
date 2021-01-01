4d606d598c2bb78bac3dc6eeea6c0098332d9f3d
AppliNH <thomas.martindev@gmail.com>
2021-01-01 15:14:18 +0100

    splitting code correcly (routes)

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,11 @@
 import express from 'express';
 import {json} from 'body-parser';
+import {currentUserRouter} from './routes/current-user';
 
 const app = express();
 app.use(json());
 
-
-app.get('/api/users/currentuser', (req,res) => {
-    res.send('Hi boi')
-});
+app.use(currentUserRouter);
 
 app.listen(3000, () => {
     console.log("Listenin' on port 3000");
---
+++ auth/src/routes/current-user.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/api/users/currentuser', () => {
+
+});
+
+
+export {router as currentUserRouter};
