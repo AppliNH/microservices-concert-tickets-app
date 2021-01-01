20c7a48d493546d5bde2bcfdce31e2e7e7588283
AppliNH <thomas.martindev@gmail.com>
2021-01-01 14:59:14 +0100

    ingress service setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,11 @@ import {json} from 'body-parser';
 const app = express();
 app.use(json());
 
+
+app.get('/api/users/currentuser', (req,res) => {
+    res.send('Hi boi')
+});
+
 app.listen(3000, () => {
     console.log("Listenin' on port 3000");
 });
