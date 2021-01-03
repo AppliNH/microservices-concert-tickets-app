5592cc8b23fc899e88ce52267c6d045c3df36ade
AppliNH <thomas.martindev@gmail.com>
2021-01-03 19:45:17 +0100

    middleware to check for jwt, for other services

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,23 +1,15 @@
 import express, { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import { BadRequestError } from '../errors/bad-request.error';
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser',
+    currentUser,
     (req: Request, res: Response) => {
-        if(!req.session?.jwt) {
-            throw new BadRequestError('Wrong credentials (no token)');
-        }
-        try {
-            const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-            res.send({currentuser: payload});
-        } catch(err) {
-            throw new BadRequestError("Couldn't validate token");
-        }
-
         
-
+        res.send({currentUser: req.currentUser || null }); // Retrieved from the middleware
 });
 
 
