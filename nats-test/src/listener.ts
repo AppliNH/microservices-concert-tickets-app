b0d026cd9ca1062549babda31e1fd43f7b4e5f0c
AppliNH <thomas.martindev@gmail.com>
2021-01-09 17:38:58 +0100

    manual ack

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -14,12 +14,19 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 stan.on("connect", () => {
     console.log('Listener connected to NATS SS !');
 
+
+    const options = stan.subscriptionOptions()
+        .setManualAckMode(true); // Listener has to manually acknowledge the received message / event
+                                // After 30s of no ack received, the server will try to send it to another instance, or just re-send it .
     // Subscribe to a channel, and a queue group.
+
     // Queue groups are made so several instances of a listener don't receive several times the same message / event
     // In short, if you have two instances of one listener "A", and if you set up a queue group, the message / event will go to one instance and not the other one
     // This is really useful, because it allow to get rid off event duplication for a same listener, which would have conducted to a double-processing.
     // Ex : on event received, the listener/app push to db. With 2 instances of your listener, it would push twice to db.
-    const subscription = stan.subscribe('ticket:created', 'ordersServiceQueueGroup');
+    
+
+    const subscription = stan.subscribe('ticket:created', 'ordersServiceQueueGroup', options);
 
     subscription.on('message', (msg: Message) => {
         console.log("Message/Event received.");
@@ -36,5 +43,7 @@ stan.on("connect", () => {
         console.log("id: ", id);
         console.log('title: ', title);
         console.log("price: ", price);
+
+        msg.ack(); // Trigger acknowledge of message
     });
 });
