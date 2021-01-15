6a3fb6e8f60677d08c6267bd27ff2b0daa543636
AppliNH <thomas.martindev@gmail.com>
2021-01-15 18:57:11 +0100

    Using nats env

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -15,6 +15,14 @@ const start = async() => {
         throw new Error("No MONGO_URI provided. Use MONGO_URI as key, as env variable.");
     }
 
+    if(!process.env.NATS_URL) {
+        throw new Error("No NATS_URL provided. Use NATS_URL as key, as env variable.");
+    }
+
+    if(!process.env.NATS_CLUSTER_ID) {
+        throw new Error("No NATS_CLUSTER_ID provided. Use NATS_CLUSTER_ID as key, as env variable.");
+    }
+
     try {
         await connect(`${process.env.MONGO_URI}/tickets`, {
             useNewUrlParser: true,
@@ -23,7 +31,7 @@ const start = async() => {
         });
 
         // Events-bus connect
-        await natsWrapper.connect("ticketing", randomBytes(4).toString('hex'), "http://nats-service:4222");
+        await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID!, process.env.NATS_URL);
 
         // Handling shutdowns gracefully
         natsWrapper.client.on('close', () => {
