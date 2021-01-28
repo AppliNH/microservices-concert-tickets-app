7d70ef8b792147fc56dcec7e66e0b0acd462ca6a
AppliNH <thomas.martindev@gmail.com>
2021-01-28 12:16:44 +0100

    order cancelled listener + tests

---
+++ payments/src/listeners/__test__/order-cancelled.listener.test.ts
@@ -0,0 +1,69 @@
+import { OrderCancelledEvent, OrderStatus } from "@react-node-microservices-course/common";
+import { natsWrapper } from "../../nats-wrapper";
+import { OrderCancelledListener } from "../order-cancelled.listener";
+import mongoose from "mongoose";
+import { Order } from "../../models/order.model";
+
+const testSetup = async () => {
+    // Creates an instance of a listener
+
+    const listener = new OrderCancelledListener(natsWrapper.client);
+
+
+
+    // Creates fake data event
+
+    const order = Order.build({
+        id: mongoose.Types.ObjectId().toHexString(),
+        price:20,
+        status: OrderStatus.Created,
+        userId: "123A"
+    });
+
+    await order.save();
+
+    const data: OrderCancelledEvent["data"] = {
+        id: order.id,
+        ticket: {
+            id: mongoose.Types.ObjectId().toHexString(),
+        },
+        __v: 1,
+    };
+
+    // Creates fake message object
+    // @ts-ignore => Make typescript compiler ignore this, and forces it to compile
+    const message: Message= {
+        ack: jest.fn() // Will allow to invoke message.ack() via mock
+    };
+
+    return { listener, data, message }
+};
+
+it("updates the order status", async() => {
+    const  { listener, data, message } = await testSetup();
+
+    await listener.onMessage(data, message);
+
+    const fetchedOrder = await Order.findById(data.id);
+
+    expect(fetchedOrder!.id).toEqual(data.id);
+    expect(fetchedOrder!.status).toEqual(OrderStatus.Cancelled)
+});
+
+
+it('acks the message', async () => {
+
+    const { listener, data, message } = await testSetup();
+
+
+    // Calls onMessage with the data object + message object
+
+    await listener.onMessage(data, message);
+
+
+    // Message is acknowledged
+
+    expect(message.ack).toHaveBeenCalled();
+
+
+});
---
+++ payments/src/listeners/order-cancelled.listener.ts
@@ -0,0 +1,38 @@
+import { Message } from "node-nats-streaming";
+import { NotFoundError, OrderCancelledEvent, OrderStatus } from "@react-node-microservices-course/common";
+import {BaseListener} from '@react-node-microservices-course/common';
+import { Subjects } from "@react-node-microservices-course/common";
+import { queueGroupName } from "./queue-group-name";
+import { Order } from "../models/order.model";
+
+
+// Will allow to lock a ticket if an order is being Cancelled for it
+export class OrderCancelledListener extends BaseListener<OrderCancelledEvent> {
+    subject:Subjects.OrderCancelled = Subjects.OrderCancelled; // This is needed to write it like that, to lock the var to the value.
+                                                                // Indeed, TicketCancelledEvent implies a subject of type Subjects.TicketCancelled
+                                                                // But you can also write it like that => readonly subject = Subjects.TicketCancelled;
+    queueGroupName = queueGroupName;
+
+
+    // onMessage callback
+    async onMessage(data: OrderCancelledEvent['data'], msg: Message) {
+        
+        const order = await Order.findOne({
+            _id: data.id,
+            __v: data.__v! -1
+        });
+
+        if (!order) {
+            throw new NotFoundError();
+        }
+
+        order.set({
+            status: OrderStatus.Cancelled
+        });
+
+        await order.save();
+        
+        msg.ack(); // Trigger acknowledge of message
+    }
+
+}
