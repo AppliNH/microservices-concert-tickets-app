bd9cbd5233d8f7ddfc82c788d4c2249ad850eeb6
AppliNH <thomas.martindev@gmail.com>
2021-01-28 17:42:48 +0100

    payment created event

--- common/src/nats-events/index.ts
+++ common/src/nats-events/index.ts
@@ -3,6 +3,7 @@ export * from './events/ticket-updated.event';
 export * from './events/order-created.event';
 export * from './events/order-cancelled.event';
 export * from './events/expiration-complete.event';
+export * from './events/payment-created.event';
 
 export * from './models/event';
 export * from './models/listener';
